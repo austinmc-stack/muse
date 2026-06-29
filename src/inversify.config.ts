4b223b66d18faa5880623a07d0c830f036087f2b
austinmc-stack <austinmcshan@gmail.com>
2026-06-29 00:25:26 -0500

    Update inversify.config.ts

--- src/inversify.config.ts
+++ src/inversify.config.ts
@@ -7,21 +7,22 @@ import ConfigProvider from './services/config.js';
 import DjTts from './services/dj-tts.js';
 import DjCommentary from './services/dj-commentary.js';
 import DjRecommender from './services/dj-recommender.js';
-
+ 
 // Managers
 import PlayerManager from './managers/player.js';
-
+ 
 // Services
 import AddQueryToQueue from './services/add-query-to-queue.js';
 import GetSongs from './services/get-songs.js';
 import YoutubeAPI from './services/youtube-api.js';
 import SpotifyAPI from './services/spotify-api.js';
-
+ 
 // Commands
 import Command from './commands/index.js';
 import Clear from './commands/clear.js';
 import Config from './commands/config.js';
 import Disconnect from './commands/disconnect.js';
+import Dj from './commands/dj.js';
 import Favorites from './commands/favorites.js';
 import ForwardSeek from './commands/fseek.js';
 import LoopQueue from './commands/loop-queue.js';
@@ -44,25 +45,25 @@ import Volume from './commands/volume.js';
 import ThirdParty from './services/third-party.js';
 import FileCacheProvider from './services/file-cache.js';
 import KeyValueCacheProvider from './services/key-value-cache.js';
-
+ 
 const container = new Container();
-
+ 
 // Intents
 const intents: GatewayIntentBits[] = [];
 intents.push(GatewayIntentBits.Guilds); // To listen for guildCreate event
 intents.push(GatewayIntentBits.GuildMessageReactions); // To listen for message reactions (messageReactionAdd event)
 intents.push(GatewayIntentBits.GuildVoiceStates); // To listen for voice state changes (voiceStateUpdate event)
-
+ 
 // Bot
 container.bind<Bot>(TYPES.Bot).to(Bot).inSingletonScope();
 container.bind<Client>(TYPES.Client).toConstantValue(new Client({intents}));
-
+ 
 // Managers
 container.bind<PlayerManager>(TYPES.Managers.Player).to(PlayerManager).inSingletonScope();
-
+ 
 // Config values
 container.bind(TYPES.Config).toConstantValue(new ConfigProvider());
-
+ 
 // Services
 container.bind<GetSongs>(TYPES.Services.GetSongs).to(GetSongs).inSingletonScope();
 container.bind<AddQueryToQueue>(TYPES.Services.AddQueryToQueue).to(AddQueryToQueue).inSingletonScope();
@@ -70,14 +71,14 @@ container.bind<YoutubeAPI>(TYPES.Services.YoutubeAPI).to(YoutubeAPI).inSingleton
 container.bind<DjTts>(TYPES.Services.DjTts).to(DjTts).inSingletonScope();
 container.bind<DjCommentary>(TYPES.Services.DjCommentary).to(DjCommentary).inSingletonScope();
 container.bind<DjRecommender>(TYPES.Services.DjRecommender).to(DjRecommender).inSingletonScope();
-
+ 
 // Only instanciate spotify dependencies if the Spotify client ID and secret are set
 const config = container.get<ConfigProvider>(TYPES.Config);
 if (config.SPOTIFY_CLIENT_ID !== '' && config.SPOTIFY_CLIENT_SECRET !== '') {
   container.bind<SpotifyAPI>(TYPES.Services.SpotifyAPI).to(SpotifyAPI).inSingletonScope();
   container.bind(TYPES.ThirdParty).to(ThirdParty);
 }
-
+ 
 // Commands
 [
   Clear,
@@ -106,9 +107,9 @@ if (config.SPOTIFY_CLIENT_ID !== '' && config.SPOTIFY_CLIENT_SECRET !== '') {
 ].forEach(command => {
   container.bind<Command>(TYPES.Command).to(command).inSingletonScope();
 });
-
+ 
 // Static libraries
 container.bind(TYPES.FileCache).to(FileCacheProvider);
 container.bind(TYPES.KeyValueCache).to(KeyValueCacheProvider);
-
+ 
 export default container;
